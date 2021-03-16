0c4652e048c13832ce51c265e3337e8a5c5d2ad6
guy2610 <80284136+guy2610@users.noreply.github.com>
2021-03-16 20:13:04 +0200

    Update label.c

--- label.c
+++ label.c
@@ -6,10 +6,10 @@
 #include "Header.h"
 #include "label.h"
 
-lbl* label_search(char* str)
+lbl* label_search(lbl* head, char* str)
 {
-	lbl* tmp = (lbl*)malloc(sizeof(lbl));
-	tmp = NULL;
+	lbl* tmp;
+	tmp = head;
 	while (tmp != NULL)
 	{
 		if (!strcmp(tmp->symbole, str))
@@ -18,9 +18,9 @@ lbl* label_search(char* str)
 			tmp = tmp->next;
 	}
 	return tmp;
-}
 
-void add_label(lbl* head, char* str, int* DC, symbol_type type)
+}
+lbl * add_label(lbl* head, char* str, int *DC, symbol_type type)
 {
 	lbl* tmp;
 	char adr[4];
@@ -29,19 +29,30 @@ void add_label(lbl* head, char* str, int* DC, symbol_type type)
 	adr[2] = '0' + (((*DC) % 100) / 10);
 	adr[3] = '0' + ((*DC) % 10);
 	(*DC) += 1;
-	tmp = label_search(head, str);
-	if (tmp == NULL)
+	tmp = head;
+	
+	if(tmp==NULL)
 	{
-		tmp = head;
+	    head=malloc(sizeof(lbl));
+	    head->symbole = malloc(MAX_LABEL_LEN * sizeof(char));
+	    head->address = malloc(4 * sizeof(char));
+		strcpy(head->symbole, str);
+		strcpy(head->address, adr);
+		head->attribute = type;
+		tmp=head->next;
+	}
+	else
+	{
 		while (tmp->next != NULL)
-			tmp = tmp->next;
-		tmp->next = malloc(sizeof(lbl));
+		    tmp = tmp->next;
+		tmp->next =malloc(sizeof(lbl)); 
 		tmp = tmp->next;
 		tmp->symbole = malloc(MAX_LABEL_LEN * sizeof(char));
-		tmp->address = malloc(MAX_LABEL_LEN * sizeof(char));
+		tmp->address = malloc(4 * sizeof(char));
 		strcpy(tmp->symbole, str);
 		strcpy(tmp->address, adr);
 		tmp->attribute = type;
-		tmp->next = NULL;
+		tmp=tmp->next;
 	}
+	return head;
 }
