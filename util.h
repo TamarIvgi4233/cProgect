2b5f065dcf67453417dcd1a4a890336cae05ce9a
guy2610 <80284136+guy2610@users.noreply.github.com>
2021-03-20 11:40:43 +0200

    Update util.h

--- util.h
+++ util.h
@@ -13,4 +13,17 @@ bool rightOperans(opcode , int );
 void extraWord(line , int , int* );
 void bin(char* , int );
 int binToDecimal(char* );
-bool is_instruction(char* str)
+bool is_instruction(char* str);
+char* HexaNumber(char* number);
+void AsciNumber(char* word, char asci);
+void registerCod(char* code, registers* reg);
+void numberCod(char* num, char* binary);
+addres_type  addressing_type(char* num);
+void op_funct_code(char* key, opcode* op, funct* fun);
+void data_free_arr();
+void cmd_free_arr();
+void label_free(lbl *head);
+char *intToString(int k);
+bool checkingEntry(lbl *head);
+bool checkingExternal(lbl *head);
+void printing_to_files(char* fileName,lbl *head ,int ICF,int DCF);
